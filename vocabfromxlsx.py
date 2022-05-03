7428ef45edbd57bb43e0fd3b18f2af9a65d1c285
Adrian Fleitas <alu0101024363@ull.edu.es>
2022-05-03 19:44:48 +0100

    part 2

--- vocabfromxlsx.py
+++ vocabfromxlsx.py
@@ -1,9 +1,11 @@
+from asyncio.windows_events import NULL
 import csv
 import spacy
 import re
 import pandas as pd
 import xlrd
 from autocorrect import Speller
+from spacymoji import Emoji
 
 def check(token, options = (1,1,1,1,1,1,1,1)):
   param = list(options)
@@ -21,13 +23,16 @@ def generateTokens(case_flag = True):
   list_tokens = []
   ignore = ('@','-','+','.',',','~','','"','\'','/','(',')','$','&',':','?','^','=')
   nlp = spacy.load("en_core_web_sm")
+  emoji = Emoji(nlp)
+  nlp.add_pipe(emoji, first=True)
   print("Parsing file")
   with open('COV_train.csv', encoding="utf-8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = pd.read_csv(csv_file, delimiter=',',compression='gzip')
     for row in csv_reader:
       for line in row:
         #checking = spell(line) #correct spelling mistakes
         doc = nlp(line)
+        if (doc._.has_emoji): doc._.emoji_desc
         for token in doc:
           if (check(token)):
             if (not token.lemma_.startswith(ignore) and not token.text[0].isdigit()):
@@ -35,6 +40,35 @@ def generateTokens(case_flag = True):
                 list_tokens.append(token.lemma_.lower())
               else:
                 list_tokens.append(token.lemma_)
+  return list_tokens
+
+def generateTokensDict(filename = 'COV_train.csv', case_flag = True):
+  list_tokens = {}
+  ignore = ('@','-','+','.',',','~','','"','\'','/','(',')','$','&',':','?','^','=')
+  nlp = spacy.load("en_core_web_sm")
+  print("Parsing file")
+  with open(filename, encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+      for line in row:
+        #checking = spell(line) #correct spelling mistakes
+        line = bytes(line, 'utf-8').decode('utf-8', 'ignore')
+        doc = nlp(line)
+        for token in doc:
+          if (check(token)):
+            if (not token.lemma_.startswith(ignore) and not token.text[0].isdigit()):
+              if (case_flag):
+                if (token.lemma_.lower() in list_tokens):
+                  iter = list_tokens.get(token.lemma_.lower())
+                  list_tokens.update({token.lemma_.lower(): iter+1})
+                else:
+                  list_tokens.update({token.lemma_.lower(): 1})
+              else:
+                if (token.lemma_ in list_tokens):
+                  iter = list_tokens.get(token.lemma_)
+                  list_tokens.update({token.lemma_: iter+1})
+                else:
+                  list_tokens.update({token.lemma_: 1})
   return list_tokens
 
 def generateVocab():
@@ -57,4 +91,4 @@ def printVocab(finalTokens):
     vocabulary.write(element + "\n")
   vocabulary.close()
 
-printVocab(generateVocab())
+#printVocab(generateVocab())
